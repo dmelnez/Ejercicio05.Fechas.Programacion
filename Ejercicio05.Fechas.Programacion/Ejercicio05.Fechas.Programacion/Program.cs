6162fa3476a591b380dd37e9c1e252d9247d66ee
David Melendez <145343465+dmelnez@users.noreply.github.com>
2024-02-05 14:01:37 +0100

    Add files via upload

---
+++ Ejercicio05.Fechas.Programacion/Ejercicio05.Fechas.Programacion/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Ejercicio05.Fechas.Programacion
+{
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("FECHA 01: ");
+            DateTime fecha01 = Convert.ToDateTime(Console.ReadLine());
+            
+
+            Console.WriteLine("FECHA 02: ");
+            DateTime fecha02 = Convert.ToDateTime(Console.ReadLine());
+
+            TimeSpan fechaDiferenciada = fecha01 - fecha02;
+
+            int dias = fechaDiferenciada.Days;
+            Console.WriteLine("DIAS: " + dias);
+
+            int horas = fechaDiferenciada.Hours;
+            Console.WriteLine("HORAS: " + horas);
+
+            int minutos = fechaDiferenciada.Minutes;
+            Console.WriteLine("MINUTOS: " + minutos);
+
+            int segundos = fechaDiferenciada.Seconds;
+            Console.WriteLine("SEGUNDOS: " + segundos);
+
+
+
+        }
+
+
+
+
+    }
+
+
+}
